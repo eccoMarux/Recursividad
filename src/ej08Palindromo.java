e3195879361fd47de3a4d02e62797b4ec942e99b
eccoMarux <mariaelvira.monserratvidal@yahoo.com>
2022-04-23 00:16:53 -0300

    Add ejercicio 8

---
+++ src/ej08Palindromo.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class ej08Palindromo {
+    public static void main(String[] args) {
+        int inicio, fin;
+        String palindromo;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese una oracion a ser evaluada como palindromo: ");
+        //ejemplo Somos o No Somos // neuquen // anita lava la tina.
+        palindromo = sc.nextLine();
+        palindromo = palindromo.replaceAll(" ", "");
+        palindromo = palindromo.toLowerCase();
+        System.out.println("True si es palindomo / False si no es palindromo: ");
+        inicio = 0;
+        fin = palindromo.length() - 1;
+        System.out.println(esPalindromo(palindromo, inicio, fin));
+        sc.close();
+    }
+
+    public static boolean esPalindromo(String oracion, int inicio, int fin) {
+        boolean palindromo=true, evaluacion;
+        int limite;
+        limite = oracion.length() / 2;
+        if (oracion.charAt(inicio) == oracion.charAt(fin)) {
+            palindromo = true;
+            if (palindromo && inicio < limite && fin > limite) {
+                evaluacion = esPalindromo(oracion, inicio + 1, fin - 1);
+                if (evaluacion == false) {
+                    palindromo = evaluacion;
+                }
+            }  
+        }else{
+            palindromo = false;
+        }
+        return palindromo;
+    }
+
+}
